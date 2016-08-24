800c86dc1b7a0eccd1568c802eb60994dae918a4
Meyyappan Nachiappan <mnachiappan1995@gmail.com>
2016-08-24 02:05:09 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((3000));
+
+// Server frontpage
+app.get('/', function (req, res) {
+    res.send('This is TestBot Server');
+});
